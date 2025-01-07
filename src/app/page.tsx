71b7589024403b5ea51766cf5864c4caf97c5702
Elizabeth Kezia Widjaja <e.kezia@gmail.com>
2025-01-07 14:41:04 +0800

    chore: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="fixed left-0 top-0 flex h-screen w-screen flex-col justify-center">
-      <h1 className="text-2xl p-4">Streets</h1>
-
-      <div className="w-full rounded-lg bg-gray-200 px-4 py-12 flex gap-8 overflow-x-scroll">
+      <div className="w-full rounded-none bg-gray-200 px-4 py-12 flex gap-8 overflow-x-scroll no-scrollbar">
         <Link href="/hong-kong">
           <h3 className="h3 hover:text-gray-300 bg-gray-500 rounded-sm px-2 text-white min-w-[100px] text-center text-nowrap">Hong Kong</h3>
         </Link>
@@ -33,6 +31,10 @@ export default function Home() {
           <h3 className="h3 hover:text-gray-300 bg-gray-500 rounded-sm px-2 text-white min-w-[100px] text-center text-nowrap"></h3>
 
       </div>
+
+     <h1 className="text-6xl p-4 font-bold text-gray-400">Streets</h1>
+      <p className="text-sm p-4 font-bold max-w-[480px] text-gray-600">A collection of choose-your-adventure stories in the form of 360 degree images, inspired by Google Streets View.</p>
+
     </main>
   );
 }
